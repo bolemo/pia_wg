ce81c60e07de3a9d2cd5a4623a07241a7f4de2ed
hELLO wORLD <grvg@free.fr>
2026-01-09 16:46:04 +0100

    Update pia_wg.sh

--- pia_wg.sh
+++ pia_wg.sh
@@ -7,7 +7,7 @@
 # - This thread: https://forum.openwrt.org/t/private-internet-access-pia-wireguard-vpn-on-openwrt/155475
 # - And @Lazerdog's script: https://github.com/jimhall718/piawg/blob/main/piawgx.sh
 #
-# Version: 1.0.14
+# Version: 1.0.15
 #
 # ©2025 bOLEMO
 # https://github.com/bolemo/pia_wg/
